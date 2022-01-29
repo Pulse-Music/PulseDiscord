d26189abb48a6d57fe04aea04ef0df4cc55fec61
Advik <advik.b@gmail.com>
2022-01-29 21:21:30 +0530

    Update run_mpv.py

--- src/run_mpv.py
+++ src/run_mpv.py
@@ -1,3 +1,5 @@
 import subprocess
 def mpv(url):
-    subprocess.Popen("start /b " + "mpv\\mpv.exe " + url + "--no-video", shell=True)
+    subprocess.Popen(f"start /b mpv\\mpv.exe {url} --no-video", shell=True)
+
+mpv('https://www.youtube.com/watch?v=BS-81QMqqd8')
