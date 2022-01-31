c938e385cd62f3de24f83b559cdbe1ce583c8842
Advik <advik.b@gmail.com>
2022-01-31 14:46:56 +0530

    Update indexer.py

--- src/indexer.py
+++ src/indexer.py
@@ -1,6 +1,5 @@
 import pytube
 import os
-from threading import Thread
 
 def search(query):
     print("Searching for " + query + "...")
@@ -11,6 +10,23 @@ def download_audio(video: pytube, cwd: str):
     print("Downloading audio...")
     stream: pytube.Stream = video.streams.filter(only_audio=True).first()
     print(stream.default_filename)
+    with open(
+        os.path.join(
+            cwd,
+            stream.default_filename.removesuffix(
+                '.mp4'
+                )
+            ),
+        'wb'
+        ) as f:
+
+        stream.stream_to_buffer(f)
+    return os.path.abspath(stream.default_filename.removesuffix('.mp4'))
+
+def download_audio_raw(url: str, cwd: str) -> str:
+    print("Downloading audio...")
+    video = pytube.YouTube(url)
+    stream = video.streams.filter(only_audio=True).first()
     with open(
         os.path.join(
             cwd,
