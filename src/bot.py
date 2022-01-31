fe4e62ab65e61e9f19ea13da9f05d12b6782ec0f
Advik <advik.b@gmail.com>
2022-01-31 14:05:59 +0530

    Update bot.py

--- src/bot.py
+++ src/bot.py
@@ -2,6 +2,9 @@ import discord
 from discord.ext import commands
 import dotenv
 import os
+import indexer
+import asyncio
+
 
 dotenv.load_dotenv()
 #NOTE: CTX = Context
@@ -30,7 +33,8 @@ async def join(ctx):
         return
     # join the voice channel
     await voice_channel.connect()
-    await ctx.send(f'Joined #{voice_channel.name}')
+    await ctx.send(f'Joined <#{voice_channel.id}>')
+    return voice_channel
 
 @client.command(aliases=['l'])
 async def leave(ctx):
@@ -40,4 +44,30 @@ async def leave(ctx):
     except AttributeError:
         await ctx.send("I am not in a voice channel")
 
+@client.command(aliases=['p'])
+async def play(ctx, *, query):
+    await ctx.send(f'Searching for {query}...')
+    # If the bot is not in a voice channel, join the channel
+    video = indexer.search(query)[0]
+    vid = indexer.download_audio(video, os.getcwd())
+    
+    try:
+        os.rename(vid, 'audio.mp3')
+    except FileExistsError:
+        os.remove('audio.mp3')
+        os.rename(vid, 'audio.mp3')
+    
+    vid = 'audio.mp3'
+    await join(ctx)
+    audio = discord.FFmpegPCMAudio(
+            executable=os.path.abspath(
+                'ffmpeg/bin/ffmpeg.exe'
+            ),
+            source=vid,
+        )
+    try:
+        ctx.voice_client.play(audio)
+    except AttributeError:
+        await ctx.send("You are in a voice channel")
+    await ctx.send(f'Playing {video.title}')
 client.run(os.getenv('BOT_TOKEN'))
