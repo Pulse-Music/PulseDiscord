5ea21e52d2c379c24347e8fa6bce2a5ad6c73a0e
Advik <advik.b@gmail.com>
2022-01-31 14:17:06 +0530

    Update bot.py

--- src/bot.py
+++ src/bot.py
@@ -3,7 +3,7 @@ from discord.ext import commands
 import dotenv
 import os
 import indexer
-import asyncio
+
 
 
 dotenv.load_dotenv()
@@ -18,57 +18,68 @@ client.recursively_remove_all_commands()
 
 @client.command()
 async def ping(ctx):
-    await ctx.send(f'Pong! {round(client.latency * 1000)}ms')
-
+    with ctx.typing():
+        await ctx.send(f'Pong! {round(client.latency * 1000)}ms')
+    return
 
 @client.command(aliases=['j'])
 async def join(ctx):
-    global voice_channels
-    # If the bot is not in a voice channel, join the channel
-    # get the voice channel the user is in
-    try:
-        voice_channel = ctx.author.voice.channel
-    except AttributeError:
-        await ctx.reply("You are not in a voice channel")
-        return None
-    # join the voice channel
-    await voice_channel.connect()
-    await ctx.send(f'Joined <#{voice_channel.id}>')
+    with ctx.typing():
+        # If the bot is not in a voice channel, join the channel
+        # get the voice channel the user is in
+        try:
+            voice_channel = ctx.author.voice.channel
+        except AttributeError:
+            await ctx.reply("You are not in a voice channel")
+            return None
+        # join the voice channel
+        await voice_channel.connect()
+        await ctx.send(f'Joined <#{voice_channel.id}>')
     return voice_channel
 
 @client.command(aliases=['l'])
 async def leave(ctx):
-    server = ctx.message.guild.voice_client
-    try:
-        await server.disconnect()
-    except AttributeError:
-        await ctx.send("I am not in a voice channel")
+    with ctx.typing():
+        server = ctx.message.guild.voice_client
+        try:
+            await server.disconnect()
+        except AttributeError:
+            await ctx.send("I am not in a voice channel")
+    return
 
-@client.command(aliases=['p'])
+@client.command(aliases=['p', 'pl'])
 async def play(ctx, *, query):
-    await ctx.send(f'Searching for {query}...')
-    # If the bot is not in a voice channel, join the channel
-    video = indexer.search(query)[0]
-    vid = indexer.download_audio(video, os.getcwd())
-    
-    try:
-        os.rename(vid, 'audio.mp3')
-    except FileExistsError:
-        os.remove('audio.mp3')
-        os.rename(vid, 'audio.mp3')
-    
-    vid = 'audio.mp3'
-    tmp = await join(ctx)
-    if tmp is None:
-        return
-    audio = discord.FFmpegPCMAudio(
-            executable=os.path.abspath(
-                'ffmpeg/bin/ffmpeg.exe'
-            ),
-            source=vid,
-        )
+    with ctx.typing():
+        await ctx.send(f'Searching for {query}...')
+        # If the bot is not in a voice channel, join the channel
+        video = indexer.search(query)[0]
+        vid = indexer.download_audio(video, os.getcwd())
+        
+        try:
+            os.rename(vid, 'audio.mp3')
+        except FileExistsError:
+            os.remove('audio.mp3')
+            os.rename(vid, 'audio.mp3')
+        
+        vid = 'audio.mp3'
+        tmp = await join(ctx)
+        if tmp is None:
+            return
+        audio = discord.FFmpegPCMAudio(
+                executable=os.path.abspath(
+                    'ffmpeg/bin/ffmpeg.exe'
+                ),
+                source=vid,
+            )
 
-    ctx.voice_client.play(audio)
-    await ctx.send(f'Playing {video.title}')
+        ctx.voice_client.play(audio)
+        await ctx.send(f'Playing {video.title}')
+    return
+
+@client.command(aliases=['o'])
+async def owner(ctx):
+    with ctx.typing():
+        ctx.send(f'My owner is <@{client.owner_id}>')
+    return
 
 client.run(os.getenv('BOT_TOKEN'))
