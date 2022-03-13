a3861fe039aca9ccadd5fef19abc9a649314d908
Advik <advik.b@gmail.com>
2022-03-13 21:25:05 +0530

    Update bot.py

--- src/bot.py
+++ src/bot.py
@@ -43,7 +43,17 @@ class MusicBot(commands.Cog):
         self.bot = bot
         self.logger = logger
         self.emoji = {
-            ""
+            "1":":one:",
+            "2":":two:",
+            "3":":three:",
+            "4":":four:",
+            "5":":five:",
+            "6":":six:",
+            "7":":seven:",
+            "8":":eight:",
+            "9":":nine:",
+            "10":":keycap_ten:",
+        }
 
     @commands.command(name='Join', aliases=['j'], help='Join the voice channel')
     async def join(self, ctx):
