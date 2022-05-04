f7bcd442d12d00948848ad46d7e062b9c607fc3f
Advik <advik.b@gmail.com>
2022-05-04 08:29:53 +0530

    Update indexer.py

--- src/modules/indexer.py
+++ src/modules/indexer.py
@@ -20,72 +20,46 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-assert __name__ != "__main__", "This module should not be run directly. Import it instead."
-
+# assert __name__ != "__main__", "This module should not be run directly. Import it instead."
 import pytube
-import os
 import py7zr
-import typing
+import requests
+import asyncio
+import os
 
-FS_NAME = "FileStorage"
+CACHE_PATH = 'cache' 
 
-
-def search_(query: str, max_results: int = 10) -> list[pytube.YouTube]:
-    yt = pytube.Search(query)
-    results = []
-    current_inc = 0
-    for result in yt.results:
-        if current_inc >= max_results:
-            break
-        if result.vid_info["videoDetails"].get("isLive") is None:
-            results.append(result)
-            current_inc += 1
-
-    return results
+def before_start():
+    """ Before the bot starts """
+    if not os.path.exists(CACHE_PATH):
+        os.mkdir(CACHE_PATH)
 
 
-def download_audio(video: pytube.YouTube, cwd: str = os.path.abspath(FS_NAME)):
-    stream: pytube.Stream = video.streams.filter(only_audio=True).first()
-    with open(os.path.join(cwd, f"{video.video_id}.mp3"), "wb") as f:
-        stream.stream_to_buffer(f)
-    # Compress the file
-    os.chdir(cwd)
-    with py7zr.SevenZipFile(f"{video.video_id}.mp3.7z", "w") as z:
-        z.write(f"{video.video_id}.mp3")
-    os.chdir("..")
-    return os.path.abspath(f"{os.path.join(FS_NAME, video.video_id)}.mp3")
+async def search(query:str, max_results:int=None):
+    """ Search for a video on YouTube """
+    if max_results is None:
+        max_results = 10
+    return pytube.YouTube(query).search(max_results=max_results)
 
+async def download_audio(video:pytube.YouTube):
+    """ Download the audio of a video """
+    audio = video.streams.filter(only_audio=True).first()
+    path = os.path.join(CACHE_PATH, video.video_id + '.mp3')
+    with open(path, 'wb') as f:
+        audio.stream_to_buffer(f)
+    return path
 
-def getinfo(video: str):
-    return pytube.YouTube(video)
+def cache_video(path:str):
+    """ Cache a video and zip it """
 
+    with py7zr.SevenZipFile(path, 'wb') as archive:
+        archive.write(file=path)
 
-def get_playlist(url: str) -> list:
-    yt = pytube.Playlist(url)
-    return list(yt.videos)
+def main():
+    before_start()
+    """ Main function """
+    video = pytube.YouTube('https://www.youtube.com/watch?v=dQw4w9WgXcQ')
+    asyncio.run(download_audio(video))
 
-
-def index_for_video_locally(
-    video: typing.Union[pytube.YouTube, str],
-    cwd: str = os.path.join(os.getcwd(), "FileStorage"),
-):
-    if type(video) is str:
-        video = pytube.YouTube(video)
-    v_id = video.video_id
-    return next(
-        (
-            os.path.join(cwd, f)
-            for f in os.listdir(cwd)
-            if v_id == f.removesuffix(".mp3.7z")
-        ),
-        None,
-    )
-
-
-def unzip(file: str) -> str:
-    os.chdir(FS_NAME)
-    with py7zr.SevenZipFile(file, "r") as z:
-        z.extractall(".")
-
-    os.chdir("..")
-    return os.path.join(FS_NAME, f"{file}.mp3")
+if __name__ == "__main__":
+    main()
