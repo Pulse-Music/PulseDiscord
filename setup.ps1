8482b1419ab9ac5c00bde49fb848b4c3dd682f8b
Advik <advik.b@gmail.com>
2022-10-08 12:56:53 +0530

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -1,5 +1,6 @@
 $download_link = "https://github.com/BtbN/FFmpeg-Builds/releases/download/latest/ffmpeg-master-latest-win64-lgpl.zip"
 $output_dirname = "ffmpeg"
+$output_zipname = "ffmpeg.zip"
 
 
 Write-Host "Making sure we have the proper directory structure" -ForegroundColor Green
@@ -14,7 +15,7 @@ Write-Host "Downloading FFmpeg..." -ForegroundColor Blue -NoNewline
 Write-Host " [This may take a while]" -ForegroundColor Yellow
 
 $progressPreference = 'silentlyContinue'
-Invoke-WebRequest $download_link -OutFile ffmpeg.zip
+Invoke-WebRequest $download_link -OutFile $output_zipname
 $progressPreference = 'Continue'
 
 Write-Host "Download complete!" -ForegroundColor Green
@@ -27,4 +28,7 @@ mv "bin/*" "../ffmpeg"
 cd ..
 Write-Host "Extraction complete!, Cleaning up" -ForegroundColor Green
 rm -r ffmpeg-master-latest-win64-lgpl
-rm ffmpeg.zip
+rm $output_zipname
+
+Write-Host "Setup complete! Press any key to exit" -ForegroundColor Green
+Read-Host
